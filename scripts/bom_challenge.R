aa0d411b2d8e81a22556c0c8a282a4f8ba0d9f12
Permalloo <per24n@csiro.au>
2019-09-26 06:48:05 +1000

    Writing steps on how to clean data in tabler and answer the question

--- scripts/bom_challenge.R
+++ scripts/bom_challenge.R
@@ -5,6 +5,24 @@ library(data.table)
 ##### Question 1 #####
 # For each station, how many days have a minimum temp, a max
 # temp, and a rainfall measurement recorded
+
+#--- Observation ---#
+# 1. Check the contents of the table - (especially the data type, column headings)
+# 2. Columns "Temp_min_max", "Rainfall", and "Solar_exposure" must be cleaned
+#    because they contain columns integer, double and other characters ( such as - and /)
+# 3. The data in the columns mentioned above is of type character.
+
+#--- Solution ---#
+#--- Data cleaning ---# 
+# 1. Split the column "Temp_min_max" into 2 columns named as "Temp_min" and "Temp_max"
+# 2. Replace all characters in "Temp_min_max", "Rainfall", and "Solar_exposure" that 
+#    are non-numeric as "na"
+# 3. Write this table as a new dataframe and save it in a new folder "Clean_Data" in 
+#    the folder "Data"
+#
+#--- Answer the question ---#
+# 1. 
+
 bom_data <- read_csv("Data/BOM_data.csv")
 bom_data
 
